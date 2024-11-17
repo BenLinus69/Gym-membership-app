1dfb80130ae3676003b40d60a614094ce2080974
Andrei <andrei_ionut200302@gmail.com>
2024-11-17 15:06:20 +0200

    added a small animation (work in progress)

--- app/src/main/java/com/example/gymmembershipapp/Gym_Progress.kt
+++ app/src/main/java/com/example/gymmembershipapp/Gym_Progress.kt
@@ -1,17 +1,27 @@
 package com.example.gymmembershipapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.*
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+
+import kotlinx.coroutines.launch
 
 class GymProgressFragment : Fragment() {
     override fun onCreateView(
@@ -26,8 +36,26 @@ class GymProgressFragment : Fragment() {
     }
 }
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun GymProgressScreen() {
+    //calc screen width
+    val screenWidth = LocalDensity.current.run { LocalConfiguration.current.screenWidthDp.dp.toPx() }
+
+    val scope = rememberCoroutineScope()
+    val animation = remember { Animatable(0f) }
+
+    //animation
+    scope.launch {
+        animation.animateTo(
+            targetValue = screenWidth,
+            animationSpec = infiniteRepeatable(
+                animation = tween(2000, easing = LinearEasing),
+                repeatMode = RepeatMode.Reverse
+            )
+        )
+    }
+
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
@@ -45,9 +73,22 @@ fun GymProgressScreen() {
             )
             Spacer(modifier = Modifier.height(16.dp))
 
-            ProgressStat(label = "Workouts Completed", value = "12")//placeholder
+            ProgressStat(label = "Workouts Completed", value = "12")
             ProgressStat(label = "Entries in the last month:", value = "4")
             ProgressStat(label = "Current Weight", value = "80")
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Canvas(modifier = Modifier.padding(start = animation.value.dp)) {
+                    drawCircle(color = Color.Blue, radius = 30f)
+                }
+            }
         }
     }
 }
